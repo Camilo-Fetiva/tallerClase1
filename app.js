e51b4bbb52e97bc0d40ece3b7507c787b507a8c8
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-29 21:44:44 -0500

    [actualizar] conexion front y back

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
 import cors from 'cors'; 
 
+// IMPORTACIONES PARA ACCEDER A LAS RUTAS DEL FRONTEND
+import path from "path";
+import { fileURLToPath } from "url";
+
 // 2. configurar el uso de nuestro servidor y dependencias
 const app = express(); 
 dotenv.config(); 
 connectionMongo();
-app.use(cors()); 
+app.use(cors());
+
+// CONFIGURACIONES PARA ACCEDER AL FRONTEND
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Le indico las rutas que debe utilizar
 app.use(express.json());
@@ -19,11 +27,13 @@ app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
 
 
-// PETICION GET (para mensaje de conexion)
-app.get(
-    '/', 
-    (req, res)=>
-        res.send('Conexion con backend funcionando correctamente'),
-);
+// PETICION PARA MOSTRAR EL FRONTEND
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 export default app;
